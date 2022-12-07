eb97dec051d843000fe525173a317e3c3e254317
SanessPL <karolpoplewski1@gmail.com>
2022-12-07 12:56:35 +0100

    Started doing welcome site

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Serwis dla artystów</title>
+    <link rel="icon" type="image/x-icon" href="logo.png">
+    <link rel="stylesheet" href="indexstyle.css">
+</head>
+<body>
+
+ <div id="main">
+    <div id="mainnapisy">
+    <h1 id="wyraz">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Wyraź <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;siebie.</h1>
+    </div>
+    <div id="onas">
+        <h2 id="onastekst"><br>O NAS<h2>
+            <p id="onasparagraf">artysci.pl to forum zrzeszające artystów na terenie całej Polski.<br> <br>To tutaj możesz zadawać
+                pytania, uzyskiwać na nie odpowiedźi, oraz odpowiadać innym. Na naszym portalu każdy znajdzie swoją 
+                kategorię - muzyk, pisarz czy rzeźbiarz. Każdy nowy członek naszej platformy sprawia, że wszyscy stajemy się lepsi.
+                <br>Jeśli też jesteś artystą, i potrzebujesz pomocy w np - uzyskaniu wernisarza -<br> zadaj pytanie na forum!
+            </p>
+    </div>
+    <div id="zarejestruj"><a href="" id="zarejestruja">ZAREJESTRUJ SIĘ!</a></div>
+    <div id="zaloguj"><a href="" id="zaloguja">ZALOGUJ SIĘ!</a></div>
+ </div>   
+ <div id="header">
+ <img src="logo.png" alt="Artysci" id="logo">
+ <p id="artysci">Artysci.pl</p>
+ </div>    
+</body>
+</html>
